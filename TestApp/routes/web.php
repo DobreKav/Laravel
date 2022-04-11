bfc305d85f434f40d2ca413391d880ec5c62fa3c
DobreKav <dobrepetruse@gmail.com>
2022-04-11 22:13:09 +0200

    update home Route

--- TestApp/routes/web.php
+++ TestApp/routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/users', function() {
-    return ['php', 'laravel', 'html'];
+    return view('home');
 });
